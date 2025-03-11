8d356388b41e936523468bb41e288d9d1aa57832
prajnaup <prajnaupadhyaya7@gmail.com>
2025-03-11 11:57:20 +0530

    commit

--- library-management-system/backend/src/models/index.js
+++ library-management-system/backend/src/models/index.js
@@ -15,15 +15,6 @@ const bookSchema = new mongoose.Schema({
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
 });
 
-const book1Schema = new mongoose.Schema({
-  book: { type: String, required: true },
-  author: { type: String, required: true },
-  published: { type: Number, required: true },
-  // genre: { type: String },
-  // price: { type: Number, required: true },
-  // reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
-});
-
 const reviewSchema = new mongoose.Schema({
   bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
@@ -38,7 +29,6 @@ const wishlistSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 const Book = mongoose.model('Book', bookSchema);
-const Book1 = mongoose.model('Book1', book1Schema);
 const Review = mongoose.model('Review', reviewSchema);
 const Wishlist = mongoose.model('Wishlist', wishlistSchema);
 
